09e2de700d8d8d4c047286accf80897804ffdba2
Colin <colin.diesh@gmail.com>
2024-10-22 00:07:45 -0400

    Works

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import {
-  createViewState,
-  JBrowseLinearGenomeView,
-} from "@jbrowse/react-linear-genome-view";
+import { createViewState } from "@jbrowse/react-linear-genome-view";
+
+import assembly from "./assembly";
+import tracks from "./tracks";
+import defaultSession from "./defaultSession";
 import Menu from "@mui/icons-material/Menu";
 
 function View() {
+  // unused, but produces issues
+  const viewState = createViewState({
+    assembly,
+    tracks,
+    defaultSession,
+  });
+
   return <Menu />;
 }
 
---
+++ src/rpcWorker.ts
@@ -0,0 +1,17 @@
+import './workerPolyfill'
+import { initializeWorker } from '@jbrowse/product-core'
+import { enableStaticRendering } from 'mobx-react'
+
+// locals
+import corePlugins from '@jbrowse/react-linear-genome-view/esm/corePlugins'
+
+// static rendering is used for "SSR" style rendering which is done on the
+// worker
+enableStaticRendering(true)
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+initializeWorker(corePlugins, {})
+
+export default function doNothing() {
+  /* do nothing */
+}
